aac037e4d94854e95e749a379785aec9d46217a0
suhaibFadl <54313934+suhaibFadl@users.noreply.github.com>
2024-01-18 07:53:59 -0800

    Home Screen update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,127 +7,207 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blueAccent,
+        items: const <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.home, size: 30),
+          Icon(Icons.analytics, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-        Container(
-        height: 150,
-        width: MediaQuery
-            .of(context)
-            .size
-            .width * 0.9,
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 32, 82, 123),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: const Column(
-          children: [
-            SizedBox(height: 24),
-            Text(
-              'Total Balance',
-              style: TextStyle(
-                color: Colors.white70,
-              ),
+          Container(
+            height: 150,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 32, 82, 123),
+              borderRadius: BorderRadius.circular(12),
             ),
-            Text(
-              '4500.00 LD',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-              ),
-            ),
-          ],
-        ),
-      ),
-      SizedBox(height: 32,),
-      Center(
-        child: Container(
-          width: MediaQuery
-              .of(context)
-              .size
-              .width * 0.9,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: const Column(
               children: [
-                Container(
-                  width: 200,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 0.5,color: Colors.grey),
-                    borderRadius: BorderRadius.circular(12),
-
+                SizedBox(height: 24),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    color: Colors.white70,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-
-                    Icon(
-                    Icons.arrow_circle_down_outlined,
-                    color: Colors.green,
-                      size: 50,
+                ),
+                Text(
+                  '4500.00 LD',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
                   ),
-                    const Text(
-                      "Income",
-                      style: TextStyle(
-                        color: Colors.black38,
-                        fontSize: 14,
-                      ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 0.5,
+                          color: const Color.fromRGBO(224, 224, 224, 1)),
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    const Text(
-                      "10.500.00 LD",
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.arrow_circle_down_outlined,
+                            color: Colors.green,
+                            size: 50,
+                          ),
+                          const Text(
+                            "Income",
+                            style: TextStyle(
+                              color: Colors.black38,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const Text(
+                            "10.500.00 LD",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0.5, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.arrow_circle_up_outlined,
+                            color: Colors.red,
+                            size: 50,
+                          ),
+                          const Text(
+                            "Expense",
+                            style: TextStyle(
+                              color: Colors.black38,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const Text(
+                            "6.000.00 LD",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Spacer(),
+          Container(
+            padding: const EdgeInsets.all(12),
+
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+            ),
+            // width: MediaQuery.of(context).size.width * 0.9,
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Transactions',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                        fontSize: 17,
                       ),
                     ),
-
-              ]),
-        ),
-
-                Container(
-                  width: 200,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 0.5,color: Colors.grey),
-
-                    borderRadius: BorderRadius.circular(12),
-
-                  ),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    Text(
+                      'See All',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.lightBlue,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.lightBlue,
+                      ),
+                      child: Icon(Icons.shopping_bag, color: Colors.blue),
+                    ),
+                    const SizedBox(width: 8),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-
-                        Icon(
-                          Icons.arrow_circle_up_outlined,
-                          color: Colors.red,
-                          size: 50,
-                        ),
-                        const Text(
-                          "Expense",
-                          style: TextStyle(
-                            color: Colors.black38,
-                            fontSize: 14,
-                          ),
-                        ),
-                        const Text(
-                          "6.000.00 LD",
+                        Text(
+                          'Payment Process',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 20,
                           ),
                         ),
-
-                      ]),
+                        Text(
+                          '4 Apr 2024',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black38,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Text(
+                      '-500',
+                      style: TextStyle(
+                        // fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
                 ),
+              ],
+            ),
+          ),
+          //
         ],
       ),
-    ),)
-    ,
-    ]
-    ,
-    )
-    ,
     );
   }
 }
